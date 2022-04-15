d3dcc123926e969eb203430be89c8d9669b42ece
yumochi <yumochi2@gmail.com>
2022-04-14 23:22:39 -0500

    added code

--- src/NotePanel/NotePanel.jsx
+++ src/NotePanel/NotePanel.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import { Button, TextField } from '@mui/material';
 import { ASSEMBO_COLORS } from "../constants";
 import {CopyToClipboard} from 'react-copy-to-clipboard';
-
+import Swal from 'sweetalert2';
+import axios from 'axios';
 
 class NotePanel extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+    window.notes = this;
   }
-
   render() {
     return (
-      <div 
+      <div
       className="containershadow"
         style={{
           padding: "5px 20px",
@@ -27,9 +28,7 @@ class NotePanel extends React.Component {
           width:"100%"
         }}
         sx={{
-
           width: { sm: 0, md: 0 },
-      
           "& .MuiOutlinedInput-root:hover": {
             "& > fieldset": {
               borderColor: "white"
@@ -61,6 +60,34 @@ class NotePanel extends React.Component {
               {this.state.copied ? "Copied!" : "Copy"}
             </Button>
         </CopyToClipboard>
+        <Button
+            onClick={ async()=>{
+              // Swal.fire({
+              //   title: 'Error!',
+              //   text: 'Do you want to continue',
+              //   icon: 'error',
+              //   confirmButtonText: 'Cool'
+              // })
+              const { value: email } = await Swal.fire({
+                title: 'Input email address',
+                input: 'email',
+                inputLabel: 'Your email address',
+                inputPlaceholder: 'Enter your email address',
+                confirmButtonColor: ASSEMBO_COLORS.primary,
+              })
+              
+              if (email) {
+                Swal.fire(`Entered email: ${email}`);
+                const result = await axios.get("http://127.0.0.1:5000/send_email", 
+                  {
+                    params: {
+                    toEmail: email,
+                    notes: this.props.note
+                  }
+                });                
+              }
+            }}
+        >Open Me</Button>
             {/* <Button
               onClick={this.props.onClickGenerateButton}
               variant="contained"
@@ -82,5 +109,4 @@ class NotePanel extends React.Component {
     );
   }
 }
-
 export default NotePanel;
