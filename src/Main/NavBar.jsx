3cdc2eb677b2b21b58fe87318d5354b3887ccf23
Sahil Maheshwari <sahilm1711@gmail.com>
2022-04-27 07:38:38 +0530

    Adding a post request and checking google sign in

--- src/Main/NavBar.jsx
+++ src/Main/NavBar.jsx
@@ -5,22 +5,26 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Button from '@mui/material/Button';
+import axios from '../axios';
 
 const google=window.google;
 
 function handleCredentialResponse(response) {
   console.log("Encoded JWT ID token: " + response.credential);
-    }
+  axios.post('/',{
+    token:response.credential
+  })
+}
 function oneTapSignIn(){
-    google.accounts.id.initialize({
+    google.accounts?.id.initialize({
           client_id: "6477792390-o5ibvubommbu11vhlglimjc0tor67reb.apps.googleusercontent.com",
           callback: handleCredentialResponse
           });
-    google.accounts.id.renderButton(
+    google.accounts?.id.renderButton(
         document.getElementById("buttonDiv"),
         { theme: "outline", size: "large" }  // customization attributes
       );
-       google.accounts.id.prompt(); // also display the One Tap dialog
+       google.accounts?.id.prompt(); // also display the One Tap dialog
   }
 
 export default function NavBar() {
