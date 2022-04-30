d4794b03c243b4d9b62d3e76ca32cbf8747104e1
ckayxian <ckayx15@gmail.com>
2022-04-30 16:17:24 -0700

    customized id and className for contact-button, copy-button, and send-email-button for the use of Google Tag Manager

--- src/NotePanel/NotePanel.jsx
+++ src/NotePanel/NotePanel.jsx
@@ -37,6 +37,8 @@ class NotePanel extends React.Component {
         <CopyToClipboard text={this.props.note}
             onCopy={() => this.setState({copied: true})}>
             <Button
+              id="NotePanel__copy-button"
+              className="NotePanel__copy-button"
               variant="contained"
               fullWidth
               style={{
@@ -56,6 +58,8 @@ class NotePanel extends React.Component {
             </Button>
         </CopyToClipboard>
         <Button
+            id="NotePanel__send-email-button"
+            className="NotePanel__send-email-button"
             variant="contained"
             fullWidth
             style={{
