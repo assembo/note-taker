b726088ed5a2e308fd010d28566f5d75023926ab
Yumo Chi <yumo.chi@reconstructinc.com>
2022-05-11 22:01:13 -0500

    removed unsed variables

--- src/Main/Home.jsx
+++ src/Main/Home.jsx
@@ -5,9 +5,8 @@ import axios from '../axios';
 import SnackbarNotification from "../Notification/Notification";
 import { BaseIconButton } from "../BaseComponents/BaseIconButton";
 import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
-import CircularProgress from '@mui/material/CircularProgress';
 import Swal from 'sweetalert2';
-import { ASSEMBO_COLORS, ASSEMBO_NOTE_TAKER_COMMANDS } from "../constants";
+import { ASSEMBO_COLORS } from "../constants";
 import "./Home.css";
 
 class Home extends React.Component {
@@ -51,19 +50,6 @@ class Home extends React.Component {
                   note: `${this.state.note}\n${text}`
                 })
               }}
-              // onClickGenerateButton={async ()=>{
-              //   const result = await axios.get("todo", {
-              //     params: {
-              //       text: this.state.note,
-              //     },
-              //   })
-              //   this.setState({
-              //     note: `${this.state.note}\n\nAction items:\n$${result.data}`,
-              //     copied: false,
-              //   })
-              //   // const result = await axios.get("http://django-tutorial-app.us-west-2.elasticbeanstalk.com")
-              //   console.log(`onClickGenerateButton`, result);
-              // }}
             ></NotePanel>
           </div>
 
