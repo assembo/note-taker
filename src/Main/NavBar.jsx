d22abca58ce0e18640f207b78a017450adcf2b87
Sahil Maheshwari <sahilm1711@gmail.com>
2022-05-15 19:08:42 +0530

    Adding zoom integration branch

--- src/Main/NavBar.jsx
+++ src/Main/NavBar.jsx
@@ -8,9 +8,12 @@ import Button from '@mui/material/Button';
 import axios from '../axios';
 import {useStyles} from './styles';
 
+
 //Error handling code in the case gscript doesn't load properly. 
 const google=window.google;
 const REACT_APP_CLIENT_ID = "6477792390-o5ibvubommbu11vhlglimjc0tor67reb.apps.googleusercontent.com"
+const ZOOM_CLIENT_ID='1xEu6h9GT6Sbd8CMUl7qCg'
+const REDIRECT_URI='https%3A%2F%2Fa4ae-27-58-74-4.in.ngrok.io'
 
 function handleCredentialResponse(response){
   axios.get('/login', { params: {'Authorization':response.credential}});
@@ -27,6 +30,7 @@ function oneTapSignIn(){
        google.accounts?.id.prompt(); // also display the One Tap dialog
   }
 
+
 export default function NavBar() {
   const classes=useStyles()
   return (
@@ -39,7 +43,8 @@ export default function NavBar() {
           <IconButton>
             <img src={logo} className={classes.AppLogo} alt="logo"/> 
           </IconButton>
-          <Button id="buttonDiv" className={classes.loginButton} onClick={()=>oneTapSignIn()}>Login</Button>  
+          <Button id="buttonDiv" className={classes.loginButton} onClick={()=>oneTapSignIn()}>Login</Button>
+          <a href={`https://zoom.us/oauth/authorize?response_type=code&client_id=${ZOOM_CLIENT_ID}&redirect_uri=${REDIRECT_URI}`}>Connect to Zoom</a>  
         </Toolbar>
       </AppBar>
     </Box>
